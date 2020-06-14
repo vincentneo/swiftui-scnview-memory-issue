37507660d9e46add5fbec12995a48524d4fa2d19
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2020-06-14 21:11:20 +0800

    Initial Commit

---
+++ SwiftUISCN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISCN
+//
+//  Created by Vincent Neo on 14/6/20.
+//  Copyright © 2020 Vincent Neo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
